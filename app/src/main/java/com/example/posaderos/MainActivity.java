56a1929084191fc042f086efdfd9d6117a9dc38e
EzefcDev <ezeferrauti@gmail.com>
2021-04-21 12:07:12 -0300

    accion del boton registrarse, permiso de internet

--- app/src/main/java/com/example/posaderos/MainActivity.java
+++ app/src/main/java/com/example/posaderos/MainActivity.java
@@ -3,15 +3,33 @@ package com.example.posaderos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button reg;
+    private String url; // declaracion de la variable para la url
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        reg = findViewById(R.id.Boton2); // asignacion de nombre para el Boton2
+
+        url = "https://www.posaderos.org/register"; // declaracion de la url de registro
+
+        //metodo boton registrarse
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 
     //metodo boton invitado
