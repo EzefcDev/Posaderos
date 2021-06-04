b4904793ce4317db29e908f19efa318cd785901c
Cristian <cristian_agareis@hotmail.com>
2021-06-03 21:19:15 -0300

    nuevos cambios

--- app/src/main/java/com/example/posaderos/MainActivity.java
+++ app/src/main/java/com/example/posaderos/MainActivity.java
@@ -6,73 +6,54 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    private Button registrase;
-    private ImageButton instagram, facebook, twitter;
+
+    private ImageButton instagram, facebook, lumen;
     // declaración de las variable para la url
-    private String urlReg, urlInst, urlFace, urlTwi;
-//hhhh
+    private String urlInst, urlFace, urlLum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // asignacion de nombre para los botones
-        registrase = findViewById(R.id.Boton2);
-        instagram = findViewById(R.id.ImBinst);
-        facebook = findViewById(R.id.ImBFace);
-        twitter= findViewById(R.id.ImBtwit);
+
+        instagram = findViewById(R.id.ImgBinst);
+        facebook = findViewById(R.id.ImgBFace);
+        lumen= findViewById(R.id.ImgBlumen);
 
         // declaracion de las url
-        urlReg = "https://www.posaderos.org/register";
+
         urlInst = "https://www.instagram.com/lumen_cor/?hl=es-la";
         urlFace = "https://www.facebook.com/lumen.cor";
-        urlTwi = "https://twitter.com/lumen_cor";
+        urlLum = "https://www.lumencor.org/";
 
-        //metodo boton registrarse y redes sociales
-        registrase.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse(urlReg);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
+        //metodo boton redes sociales
+        instagram.setOnClickListener(view -> {
+            Uri uri = Uri.parse(urlInst);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
         });
 
-        instagram.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse(urlInst);
+        facebook.setOnClickListener(view -> {
+             Uri uri = Uri.parse(urlFace);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+         });
+
+        lumen.setOnClickListener(view -> {
+                Uri uri = Uri.parse(urlLum);
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
-            }
-        });
-
-        facebook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                 Uri uri = Uri.parse(urlFace);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-             }
-        });
-
-        twitter.setOnClickListener(new View.OnClickListener() {
-            @Override
-                public void onClick(View view) {
-                    Uri uri = Uri.parse(urlTwi);
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-            }
         });
     }
 
     //metodo boton invitado
-     public void Invitado (View view){
-     Intent invitado = new Intent(this,Alerta.class);
+     public void GenerarSoS (View view){
+     Intent invitado = new Intent(this,carga_datos1.class);
      startActivity(invitado);
     }
 
